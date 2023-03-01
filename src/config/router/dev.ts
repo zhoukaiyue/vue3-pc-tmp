0bbac47bd7f0b52df201c46071efb413c0af4f92
zhoukaiyue <1301524439@qq.com>
2023-03-01 12:08:03 +0800

    feat：开发者中心新增 全局plugin使用demo

--- src/config/router/dev.ts
+++ src/config/router/dev.ts
@@ -4,7 +4,7 @@
  * @Author: zhoukai
  * @Date: 2022-12-06 16:14:33
  * @LastEditors: zhoukai
- * @LastEditTime: 2023-02-02 17:08:34
+ * @LastEditTime: 2023-03-01 11:54:53
  */
 const devLayOut = () => import('@/layout/devLayout/index.vue');
 /**
@@ -12,7 +12,7 @@ const devLayOut = () => import('@/layout/devLayout/index.vue');
  * icon官网：https://xicons.org/#/
  * 添加方法：选择 ionicons5 并 Copy Name，在下方 import
  */
-import { LogoWebComponent, LogoCss3, InformationCircleSharp, Storefront } from '@vicons/ionicons5';
+import { LogoWebComponent, LogoCss3, InformationCircleSharp, Storefront, Egg } from '@vicons/ionicons5';
 const routes = [
     {
         path: '/dev',
@@ -59,6 +59,16 @@ const routes = [
                 },
 
                 component: () => import('@/views/dev/pinia/index.vue')
+            },
+            {
+                path: 'plugin',
+                name: 'plugin',
+                meta: {
+                    title: 'plugin',
+                    icon: Egg
+                },
+
+                component: () => import('@/views/dev/plugin/index.vue')
             }
         ]
     }
---
+++ src/views/dev/plugin/index.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: zhoukai
+ * @Date: 2023-03-01 11:54:00
+ * @LastEditors: zhoukai
+ * @LastEditTime: 2023-03-01 12:06:44
+-->
+<script setup lang="ts">
+/**
+ * 打开message信息弹窗
+ */
+const openMessage = () => {
+    /**
+     * 以 warning 为例,支持官方 [MessageProvider Props](https://www.naiveui.com/zh-CN/os-theme/components/message#MessageProvider-Props)
+     */
+    window.$message.warning('How many roads must a man walk down');
+};
+
+/**
+ * 打开可关闭的message信息弹窗
+ */
+const createMessage = () => {
+    /**
+     * 以 info 为例,支持官方 [MessageProvider Props](https://www.naiveui.com/zh-CN/os-theme/components/message#MessageProvider-Props)
+     */
+    window.$message.info("I don't know why nobody told you how to unfold your love", {
+        closable: true,
+        duration: 5000
+    });
+};
+</script>
+<template>
+    <div>
+        <n-divider title-placement="left">信息 Message</n-divider>
+        <n-button type="primary" @click="openMessage">信息 Message</n-button>
+        <n-button ml-20px type="primary" @click="createMessage">信息 Message</n-button>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
