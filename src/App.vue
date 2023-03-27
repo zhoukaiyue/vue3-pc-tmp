355e40e36d8c44526419fd96ddc8ec9b9a56eeee
zhoukaiyue <1301524439@qq.com>
2023-03-27 10:17:01 +0800

    feat: 支持keepAlive缓存指定路由组件
    使用方式： 在需要缓存的路由元信息中添加 keepAlive:true 即可

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,18 @@
  * @Author: zhoukai
  * @Date: 2022-10-26 16:42:22
  * @LastEditors: zhoukai
- * @LastEditTime: 2023-03-27 10:06:23
+ * @LastEditTime: 2023-03-27 10:15:27
 -->
 <script setup lang="ts"></script>
 
 <template>
-    <router-view v-slot="{ Component }">
-        <component :is="Component" />
+    <router-view v-slot="{ Component, route }">
+        <!-- 缓存的视图 -->
+        <keep-alive>
+            <component v-if="route.meta.keepAlive" :is="Component" :key="route.name || route.fullPath" />
+        </keep-alive>
+        <!-- 不缓存的视图 -->
+        <component v-if="!route.meta.keepAlive" :is="Component" />
     </router-view>
 </template>
 
