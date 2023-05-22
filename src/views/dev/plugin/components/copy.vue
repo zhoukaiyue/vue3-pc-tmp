9b4b9f7ff2f9c533a3e4f5106f88532765dfe753
zhoukaiyue <1301524439@qq.com>
2023-05-22 14:24:01 +0800

    feat: add vue-clipboard3 插件

---
+++ src/views/dev/plugin/components/copy.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import useClipboard from 'vue-clipboard3';
+const { toClipboard } = useClipboard();
+const text = ref('复制到剪贴板的内容');
+
+const copy = async () => {
+    try {
+        await toClipboard(text.value);
+        window.$message.success('Copied to clipboard');
+    } catch (e) {
+        console.error(e);
+    }
+};
+</script>
+<template>
+    <n-button size="small" plain type="primary" @click="copy">复制到剪贴板</n-button>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/dev/plugin/index.vue
+++ src/views/dev/plugin/index.vue
@@ -4,43 +4,18 @@
  * @Author: zhoukai
  * @Date: 2023-03-01 11:54:00
  * @LastEditors: zhoukai
- * @LastEditTime: 2023-03-27 10:08:12
+ * @LastEditTime: 2023-05-22 14:20:36
 -->
 <script setup lang="ts">
-import { getCurrentInstance } from 'vue';
-import type { ComponentInternalInstance } from 'vue';
-const { appContext } = getCurrentInstance() as ComponentInternalInstance;
-
-/**
- * 打开message信息弹窗
- * TODO:以vue插件的形式调用
- */
-const openMessage = () => {
-    /**
-     * 以 warning 为例,支持官方 [MessageProvider Props](https://www.naiveui.com/zh-CN/os-theme/components/message#MessageProvider-Props)
-     */
-    appContext.config.globalProperties.$message.warning('How many roads must a man walk down');
-};
-
-/**
- * 打开可关闭的message信息弹窗
- * TODO:通过window方法调用
- */
-const createMessage = () => {
-    /**
-     * 以 info 为例,支持官方 [MessageProvider Props](https://www.naiveui.com/zh-CN/os-theme/components/message#MessageProvider-Props)
-     */
-    window.$message.info("I don't know why nobody told you how to unfold your love", {
-        closable: true,
-        duration: 5000
-    });
-};
+import message from './components/message.vue';
+import copy from './components/copy.vue';
 </script>
 <template>
     <div>
         <n-divider title-placement="left">信息 Message</n-divider>
-        <n-button type="primary" @click="openMessage">信息 Message</n-button>
-        <n-button ml-20px type="primary" @click="createMessage">信息 Message</n-button>
+        <message></message>
+        <n-divider title-placement="left">copy</n-divider>
+        <copy></copy>
     </div>
 </template>
 
