58fc1cfb544e0107a228b9ce807fdae71f9f4cd2
zhoukaiyue <1301524439@qq.com>
2024-01-22 10:25:01 +0800

    ci：update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@
  * @Author: zhoukai
  * @Date: 2022-10-26 16:42:22
  * @LastEditors: zhoukai
- * @LastEditTime: 2023-02-08 14:42:53
+ * @LastEditTime: 2024-01-22 10:24:43
  */
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
@@ -26,6 +26,7 @@ module.exports = {
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
         'vue/component-definition-name-casing': 'off', // 对组件命名不做校验，你可以采用驼峰命名或者烤肉串大小写。
-        'vue/multi-word-component-names': 'off' //关闭eslint检查文件名是否为驼峰命名
+        'vue/multi-word-component-names': 'off', //关闭eslint检查文件名是否为驼峰命名
+        '@typescript-eslint/no-unused-vars': 'error' // 对未使用的变量进行检查，如果未使用则报错
     }
 };
